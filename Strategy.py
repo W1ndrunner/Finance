b9fa2d0da52868435c572c372d7d95d10cd3b210
W1ndrunner <lewislanek3264@gmail.com>
2023-07-03 22:44:08 +0100

    Commented

--- Strategy.py
+++ Strategy.py
@@ -1,14 +1,21 @@
 import csv
+
+# Arrays for data needed
 Dates = []
 Prices = []
+
+# Reading file and storing data in arrays
 with open("MSFT.csv", 'r') as file:
     csvreader = csv.reader(file)
     for row in csvreader:
         Dates.append(row[0])
         Prices.append(row[4])
 
+# Deleting Names of columns
 del Dates[0]
 del Prices[0]
 
+SMA = []
+EMA = []
 
 
