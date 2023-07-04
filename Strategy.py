1f578025ee0e6014a47df662c9d9171ff35300cc
W1ndrunner <lewislanek3264@gmail.com>
2023-07-04 23:07:05 +0100

    SMA done

--- Strategy.py
+++ Strategy.py
@@ -7,7 +7,7 @@ Data = []
 class Info:
     def __init__(self, date, price):
         self.date = date
-        self.price = price
+        self.price = float(price)
     
     def getDate(self):
         return self.date
@@ -19,11 +19,11 @@ class Info:
 with open("MSFT.csv", 'r') as file:
     csvreader = csv.reader(file)
     for row in csvreader:
+        if row[0] == 'Date':
+            continue
         tempObj = Info(row[0], row[4])
         Data.append(tempObj)
 
-# Deleting names of columns
-del Data[0]
 
 # Arrays to store SMA and EMA objects
 SMA1 = []
@@ -33,18 +33,24 @@ EMA1 = []
 EMA2 = []
 
 # Calculates SMA
-def CalcSMA(day):
+def CalcSMA(day, selector):
     counter = day
     days = []
     for i in range(0,day):
         days.append(Data[i])
-    while Data[counter] != None:
+    while counter < len(Data):
         total = 0
         for ele in days:
             total += ele.getPrice()
         SMA = total/day
         temp = Info(Data[day-1].getDate(), SMA)
-        SMA1.append(temp)
+        if selector == 1:
+            SMA1.append(temp)
+        else:
+            SMA2.append(temp)    
         del days[0]
         days.append(Data[counter])
         counter += 1
+
+CalcSMA(10, 1)
+print(SMA1[0].getPrice())
